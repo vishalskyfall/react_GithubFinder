20c9568bd9f532bef10fdcea3fa633ed34061ab9
Vishal Brahmbhatt <brahmbhattvishal4016@gmail.com>
2022-03-22 11:33:03 +0530

    comment

--- src/context/GithubContext.js
+++ src/context/GithubContext.js
@@ -4,7 +4,7 @@ import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 // console.log(GITHUB_TOKEN +" "+GITHUB_URL);
 export const GithubProvider = ({ children }) => {
   // const [users, setUsers] = useState([]);
